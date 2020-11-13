b84ec2f44673f1ccdf42292cae0ae1057030fc40
crismariano <cris.mariano6@gmail.com>
2020-11-13 14:53:40 -0500

    commit 6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Use d3.json() to fetch data from JSON file
 // Incoming data is iternally referred to as incomingData
 
-function buildPlot() {
+function buildPlot(id) {
     d3.json("data/samples.json").then(function(bellyData) {
         //console.log(bellyData);
         // Grab values from the samples.json object to built the horizontal Bar Chart
@@ -19,6 +19,8 @@ function buildPlot() {
         // Top 10 otu_labels for hovetext for chart
         var sampleLabel = labels.slice(0, 10);
         //console.log(sampleLabel);
+        //var metadata = bellyData.metadata
+        //console.log(metadata);
     
         // Trace for Bar Chart
         var trace = {
@@ -71,6 +73,59 @@ function buildPlot() {
     
         // Bubble Plot
         Plotly.newPlot("bubble", data1, layout1);
-    });
+    })
 }
 buildPlot()
+
+// Demographic Data
+function demoData () {
+    d3.json("data/samples.json").then(function(demo) {
+        //console.log(demo);
+
+        var metadata = demo.metadata;
+        console.log(metadata);
+
+        // Create empty arrays to store metadata information
+        var ids = [];
+        var ethnicities = [];
+        var genders = [];
+        var ages = [];
+        var locations = [];
+        var bbtypes = [];
+        var wfreqs = [];
+
+        // Iterate through each key and value
+        metadata.forEach((meta) => {
+            //Iterate through each metadata object
+            Object.entries(meta).forEach(([key, value]) => {
+                //Use the key to determine hich array to push value to
+                if (key === "id") {
+                    ids.push(value);
+                }
+                if (key === "ethnicity") {
+                    ethnicities.push(value);
+                }
+                if (key === "gender") {
+                    genders.push(value);
+                }
+                if (key === "age") {
+                    ages.push(value);
+                }
+                if (key === "location") {
+                    locations.push(value);
+                }
+                if (key === "bbtype") {
+                    bbtypes.push(value);
+                }
+                if (key === "wfreq") {
+                    wfreqs.push(value);
+                }
+            });
+        });
+    })
+
+}
+demoData()
+
+
+
